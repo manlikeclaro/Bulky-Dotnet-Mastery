0ef4613415436675570d31ff5488c20a9a0786c3
manlikeclaro <ckibii08@gmail.com>
2024-06-27 21:21:46 +0300

    [feat] made migrations for ctegories table

--- BulkyWeb/Data/ApplicationDbContext.cs
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BulkyWeb.Data;
 
@@ -6,6 +7,7 @@ public class ApplicationDbContext : DbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
-        
     }
+
+    public DbSet<Category> Categories { get; set; }
 }
